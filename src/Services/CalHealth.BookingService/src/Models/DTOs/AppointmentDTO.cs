bfa51267a9a99d9bcb1099f98f72da2f49f6cd40
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-07-26 12:45:50 +0200

    Booking: more granular error responses

--- src/Services/CalHealth.BookingService/src/Models/DTOs/AppointmentDTO.cs
+++ src/Services/CalHealth.BookingService/src/Models/DTOs/AppointmentDTO.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CalHealth.BookingService.Models
 {
     public class AppointmentDTO
     {
         public int Id { get; set; }
+        
+        [Range(1, int.MaxValue)]
         public int TimeSlotId { get; set; }
+        
+        [Range(1, int.MaxValue)]
         public int ConsultantId { get; set; }
+        
         public DateTime Date { get; set; }
 
+        [Required]
         public PatientDTO Patient { get; set; }
     }
 }
